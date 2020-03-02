f7539584ff0cb2bf95b1509e614a4ce2f6710c9d
NateChris <divineconclusion@gmail.com>
2020-03-01 19:24:28 -0500

    Logout view function added.

--- app/views.py
+++ app/views.py
@@ -64,6 +64,12 @@ def login():
 def secure_page():
     return render_template('secure_page.html')
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash("You've been logged out: Have a great day!", 'info')
+    return redirect(url_for('home'))
+
 
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
